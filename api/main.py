e2cd57dfda98f6f4563ac2cf753e126053978a78
pdharshinij59-Vara <pdharshinij59@gmail.com>
2026-03-08 19:59:23 -0400

    Add redirect endpoint for short URLs

--- api/main.py
+++ api/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
-from services.shortener import create_short_url
+from fastapi.responses import RedirectResponse
+from services.shortener import create_short_url, get_long_url
 
 app = FastAPI()
 
@@ -17,3 +18,13 @@ def home():
 def shorten_url(request: URLRequest):
     result = create_short_url(request.url)
     return result
+
+
+@app.get("/{code}")
+def redirect_to_url(code: str):
+    long_url = get_long_url(code)
+
+    if long_url:
+        return RedirectResponse(long_url)
+
+    return {"error": "URL not found"}
