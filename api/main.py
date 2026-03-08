5084602bc7d5380141f9915fd12354acc3429850
pdharshinij59-Vara <pdharshinij59@gmail.com>
2026-03-08 19:51:20 -0400

    Connect API endpoint to URL shortener service

--- api/main.py
+++ api/main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from services.shortener import create_short_url
 
 app = FastAPI()
+
+class URLRequest(BaseModel):
+    url: str
+
 
 @app.get("/")
 def home():
     return {"message": "URL Shortener System Running"}
+
+
+@app.post("/shorten")
+def shorten_url(request: URLRequest):
+    result = create_short_url(request.url)
+    return result
