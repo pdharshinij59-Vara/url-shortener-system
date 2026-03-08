47cd3328201843886f4f4a1e89200331953738f8
pdharshinij59-Vara <pdharshinij59@gmail.com>
2026-03-08 19:57:33 -0400

    Add in-memory URL storage

--- api/services/shortener.py
+++ api/services/shortener.py
@@ -3,16 +3,29 @@ import random
 
 BASE_URL = "http://localhost:8000/"
 
+# temporary storage
+url_database = {}
+
 def generate_short_code(length=6):
     characters = string.ascii_letters + string.digits
     short_code = ''.join(random.choice(characters) for _ in range(length))
     return short_code
 
+
 def create_short_url(long_url):
     code = generate_short_code()
+
+    # store mapping
+    url_database[code] = long_url
+
     short_url = BASE_URL + code
+
     return {
         "long_url": long_url,
         "short_url": short_url,
         "code": code
     }
+
+
+def get_long_url(code):
+    return url_database.get(code)
