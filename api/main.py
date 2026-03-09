a1e722ce009b9c526fa4948a98cde3703d92d374
pdharshinij59-Vara <pdharshinij59@gmail.com>
2026-03-08 20:14:41 -0400

    Connect API endpoints to database session

--- api/main.py
+++ api/main.py
@@ -1,12 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+from sqlalchemy.orm import Session
 from pydantic import BaseModel
 from fastapi.responses import RedirectResponse
+
 from services.shortener import create_short_url, get_long_url
+from database.db import SessionLocal, init_db
 
 app = FastAPI()
 
+init_db()
+
+
 class URLRequest(BaseModel):
     url: str
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 
 @app.get("/")
@@ -15,14 +29,14 @@ def home():
 
 
 @app.post("/shorten")
-def shorten_url(request: URLRequest):
-    result = create_short_url(request.url)
+def shorten_url(request: URLRequest, db: Session = Depends(get_db)):
+    result = create_short_url(db, request.url)
     return result
 
 
 @app.get("/{code}")
-def redirect_to_url(code: str):
-    long_url = get_long_url(code)
+def redirect_to_url(code: str, db: Session = Depends(get_db)):
+    long_url = get_long_url(db, code)
 
     if long_url:
         return RedirectResponse(long_url)
